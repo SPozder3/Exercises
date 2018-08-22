d1aa41dfc208d394e0ce596e542f5eb61addc829
Scott Alan Pozder <pozdersa@s.dcsdk12.org>
2018-08-22 09:10:27 -0600

    change the code to ask for user input

--- AreaCircle.java
+++ AreaCircle.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class AreaCircle{
 	public static void main(String[] args){
-		int radius = 3;
+		int radius;
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Input the radius: ");
+		radius = scan.nextInt();
+
 		double area = Math.PI * radius * radius;
-		System.out.println("The radius is: " + radius + " The area is: " + area);
+		System.out.println("The area is: " + area);
 	}
 }
