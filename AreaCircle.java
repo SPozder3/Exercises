59eeeca79225c3ea1651076c8a6a87256b328789
Scott Alan Pozder <pozdersa@s.dcsdk12.org>
2018-08-22 08:56:48 -0600

    code a program that calculates the area of a circle given the radius

---
+++ AreaCircle.java
@@ -0,0 +1,7 @@
+public class AreaCircle{
+	public static void main(String[] args){
+		int radius = 3;
+		double area = Math.PI * radius * radius;
+		System.out.println("The radius is: " + radius + " The area is: " + area);
+	}
+}
