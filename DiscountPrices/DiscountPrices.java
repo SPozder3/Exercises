afc9b7a3e3b52dff633f8187484dd4f31a3c9feb
Spozder3 <scottpozder@gmail.com>
2018-08-30 23:24:45 -0600

    create a program that finds that discounted price of something that costs more than 10 dollars

---
+++ DiscountPrices/DiscountPrices.java
@@ -0,0 +1,18 @@
+import util.java.Scanner;
+
+public class DiscountPrices{
+	public static void main(String[] args){
+		int cost;
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Enter amount of purchases:");
+		cost = scan.nextInt;
+
+		if (cost > 10){
+			int discount = cost - (cost * 0.1);
+			System.out.println("Discounted price:" + discount);
+		} else {
+			System.out.println("Discount not avaliable")
+		}
+	}
+}
