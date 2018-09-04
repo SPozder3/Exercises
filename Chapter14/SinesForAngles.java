acb90ee80eec754d9a8afa5ea45b6c9810f66583
SPozder3 <scottpozder@gmail.com>
2018-09-04 07:37:20 -0600

    move files into corresponding directories

--- Chapter14/SinesForAngles.java
+++ Chapter14/SinesForAngles.java
@@ -6,7 +6,7 @@ public class SinesForAngles{
 		DecimalFormat zero = new DecimalFormat("0.0");
 		double angle = 105.0;
 		
-		while (angle >= -90.0){
+		while (angle >= -75.0){
 			angle = angle - 15;
 			double radians = angle * Math.PI/180;
 			double sinx = Math.sin(radians);
@@ -14,11 +14,11 @@ public class SinesForAngles{
 			if (angle <= -15.0 && angle >= -90.0){
 				System.out.println(angle + "\t" + form.format(sinx));
 			}
-			if (angle <= 90.0 && angle >= 15.0){
+			else if (angle != 0.0){
 				System.out.println(" " + angle + "\t" + " " + form.format(sinx));
 			}
-			if (angle == 0.0){
-				System.out.println("  " + angle + "\t" + " " + form.format(sinx));
+			else{
+				System.out.println("  " + angle + "\t" + " " + zero.format(sinx));
 			}
 
 		}
