f3a5b4edb1211bd729d9536356e92443ceec4546
SPozder3 <scottpozder@gmail.com>
2018-09-25 08:27:55 -0600

    not finished

---
+++ OldVersion/Chapter17/Permutations/Permutations.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Permutations{
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Enter N:");
+		int n = nextInt();
+		System.out.println("Enter R:");
+		int r = nextInt();
+
+		if (n < 0 || r < 0 || r > n){
+			System.out.println("Both must be 0 or postitive and R must be less than or equal to N");
+		}
+		else {
+			
+		}
+	}
+}
