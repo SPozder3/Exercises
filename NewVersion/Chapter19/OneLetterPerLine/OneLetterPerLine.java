4f8f674d1296c0abbda38cf4e38cb6fb35171059
Spozder3 <scottpozder@gmail.com>
2018-09-25 22:08:55 -0600

    write a program writes each letter of a word

---
+++ NewVersion/Chapter19/OneLetterPerLine/OneLetterPerLine.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class OneLetterPerLine{
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a word:");
+		String word = scan.nextLine();
+
+		int x = 1;
+		while (x < word.length()){
+			System.out.println(word.charAt(x));
+			x = x + 1;
+		}
+	}
+}
