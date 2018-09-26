e0d3bea0979f32785d26e6e177d183c78a59cd74
Spozder3 <scottpozder@gmail.com>
2018-09-25 21:26:20 -0600

    write a program that asks the user for the low and high integer in a range and then finds the sum in and out of that range

---
+++ OldVersion/InRangeAdder/InRangeAdder.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class InRangeAdder{
+	public static void main(String[] args){
+		System.out.println("In-range Adder");
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Low end of range:");
+		int low = scan.nextInt();
+		System.out.println("High end of range:");
+		int high = scan.nextInt();
+
+		int sumInRange = 0;
+		int sumOutRange = 0;
+		int data = 1;
+		while (data != 0){
+			System.out.println("Enter data:");
+			data = scan.nextInt();
+			if (data > low && data < high){
+				sumInRange = sumInRange + data;
+			}
+			else{
+				sumOutRange = sumOutRange + data;
+			}
+
+		System.out.println("Sum of in range values: " + sumInRange);
+		System.out.println("Sum of out of range values: " + sumOutRange);
+		}
+	}
+}
