2707287aa04d7ffc8ac077d08aceff7e5235dde9
Spozder3 <scottpozder@gmail.com>
2018-09-25 22:25:05 -0600

    write a program that writes out the value of n starting at 0 to 25

---
+++ NewVersion/Chapter24/SheepHerdSize/SheepHerdSize.java
@@ -0,0 +1,8 @@
+public class SheepHerdSize{
+	public static void main(String[] args){
+		for (years = 0; years < 25; years++){
+			double n = 220/(1 + 10 * Math(0.83,years);
+			System.out.println(n);
+		}
+	}
+}
