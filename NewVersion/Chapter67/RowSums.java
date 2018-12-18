bdd02b79bee214753d9fa4bf6d72382b88406263
Spozder3 <scottpozder@gmail.com>
2018-12-17 19:57:30 -0700

    Chapter 67 exercises

--- NewVersion/Chapter67/RowSums.java
+++ NewVersion/Chapter67/RowSums.java
@@ -1,9 +1,9 @@
 import java.io.* ;
 
-public class ArraySum
+public class RowSums
 {
 
-  public static void main ( String[] args )  
+  public static void main ( String[] args ) 
   {
     int[][] data = { {3, 2, 5},
                      {1, 4, 4, 8, 13},
@@ -11,18 +11,22 @@ public class ArraySum
                      {0, 2, 6, 3, -1, -8} };
     
     // declare the sum
-    int sum = 0;
+    int sum;
     
-    // compute the sum
+    // compute the sums for each row
     for ( int row=0; row < data.length; row++)
     {
+      // initialize the sum
+      sum = 0;
+      
+      // compute the sum for this row
       for ( int col=0; col < data[row].length; col++) 
       {
-        sum += data[row][col];
+         sum += data[row][col];
       }
-    }
       
-    // write out the sum
-    System.out.println(sum);
+      // write the sum for this row
+      System.out.println("The sum for row " + row + " is " + sum);
+    }
   }
-}     
+}
